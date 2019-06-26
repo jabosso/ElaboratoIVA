6dbd1da54c5b4cbea0f9736a217b4a33d4604031
jabosso <bossojohan@gmail.com>
2019-06-26 09:00:30 +0200

    modi

--- test2.py
+++ test2.py
@@ -21,35 +21,53 @@ def let_me_see(df):
 
     :param matrix: matrix of body landmarks with dim=[Nframes, Nlandmarks,(x,y)]
     """
-    #print( np.amax(df['frame']) )
-
     for i in range(max(df['frame'])+1):
         bframe = np.zeros((650, 650, 3), np.uint8)
-        overlay = bframe
         d=df.loc[df['frame'] == i]
-        x=d['x']
-        y=d['y']
-        for j in range(x.shape[0]):
-            cv2.circle(overlay, (x.iloc[j], y.iloc[j]), 5, [255,0,0], -1)
-        cv2.imshow('output',overlay)
-        for element in body.connection:
-            #da mettere controllo sull'esistenza del punto nel dataframe e quindi della connection
-            #if(d.loc[ d['body_part']==body.dictionary[element[0]]]):
-            tmp= d.loc[d['body_part']==body.dictionary[element[0]]]
-            point_a=(tmp['x'],tmp['y'])
-            tmp=d.loc[d['body_part']==body.dictionary[element[1]]]
-            point_b=(tmp['x'],tmp['y'])
-            cv2.line(bframe, point_a, point_b, color[element[0]], 4)
-        alpha = 0.3
-        cv2.addWeighted(overlay, alpha, bframe, 1 - alpha, 0, bframe)
+        bframe = body_plot(bframe,d)
         cv2.imshow('output', bframe)
         time.sleep(0.2)
         k = cv2.waitKey(1)
         if k == 27:
             break;
 
+def body_plot(frame,d):
+    x = d['x']
+    y = d['y']
+    for j in range(x.shape[0]):
+        cv2.circle(frame, (x.iloc[j], y.iloc[j]), 5, [255, 0, 0], -1)
+    for el in body.connection:
+        d_A = d.loc[d['body_part'] == body.dictionary[el[0]]]
+        d_B = d.loc[d['body_part'] == body.dictionary[el[1]]]
+        try:
+            _ = d_A['body_part'].item()
+            _ = d_B['body_part'].item()
+            point_a = (d_A.x.item(), d_A.y.item())
+            point_b = (d_B.x.item(), d_B.y.item())
+            cv2.line(frame, point_a, point_b, color[el[0]], 4)
+        except:
+            _ = ''
+    return frame
+
+def let_me_see_two_movements(df1, df2):
+    for i in range(max(df1['frame'])+1):
+        bframe1 = np.zeros((650, 650, 3), np.uint8)
+        bframe2 = bframe1
+        d1=df1.loc[df1['frame'] == i]
+        d2 = df2.loc[df2['frame'] == i]
+        bframe1 = body_plot(bframe1,d1)
+        bframe2 = body_plot(bframe2, d2)
+        alpha = 0.3
+        cv2.addWeighted(bframe2, alpha, bframe1, 1 - alpha, 0, bframe1)
+        cv2.imshow('output', bframe1)
+        time.sleep(0.2)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break;
+
+
 let_me_see(data)
 
-#def let_me_see_two_movements(matrix1, matrix2):
+
 #sincro
 #let me see_sincro
